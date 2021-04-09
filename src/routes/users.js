c48f5edf5f4e705a80ff85c9e0b38906fac0355f
Youssef Elshemi <ye216@exeter.ac.uk>
2021-04-09 12:16:06 +0100

    fixes and meta stuff

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 
-const User = require('../models/User');
+const User = require('../models/user');
 const { forwardAuthenticated } = require('../config/auth');
 
 const router = express.Router();
